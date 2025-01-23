1cad4bb699e80a6538a2565143504bfde860ce39
joshua <saavedrajoshua760@gmail.com>
2025-01-23 17:04:25 +0800

    act 1 hero crouse

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ const App = () => {
   return <>
     <div className='wrapper'>
       <div className='hero-img-wrapper'>
-        <img className='hero-img' src="/joshua.png" alt="Joshua Saavedra portrait" />
+        <img className='hero-img' src="/josh.png" alt="Joshua Saavedra portrait" />
       </div>
       <div className='info-wrapper'>
         <div>
           <p className='greeting'><span className='line'></span>Hello</p>
           <h1 className='intro'>I am <span className='highlight-bold'>Joshua</span> Saavedra</h1>
-          <h2 className='desc'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas eaque quia hic ratione id dolorum impedit quae nesciunt cum molestias.</h2>
+          <h2 className='desc'>I’m passionate about designing solutions that prioritize sustainability, innovation, and community impact. Through thoughtful, eco-conscious projects, I strive to reduce environmental footprints while inspiring a more conscious way of living and working.</h2>
           <button className='dl-cv'>Download CV</button>
         </div>
       </div>
