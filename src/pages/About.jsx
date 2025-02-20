86df21b354d72fd1bfe961abe5995a271b36bb86
joshua <saavedrajoshua760@gmail.com>
2025-02-20 21:59:25 +0800

    Your commit message here

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <img src='/josh.png' alt='Joshua Saavedra' className='about-img' />
         <div className='about-content'>
           <h1>Joshua Saavedra <span className='wave-icon'>👋</span></h1>
-          <h2>BSIT Student at WMSU</h2>
+          <h2>BSIT Student at Western Mindanao State University</h2>
           <h3>Aspiring Web Developer</h3>
           <p>
             Passionate about building modern web applications that enhance user experiences and make a positive impact on the world. 
